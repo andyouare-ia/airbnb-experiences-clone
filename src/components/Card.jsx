264654055107608dd1197e349b4bb950c4a82373
Nuria Toledano <ia.andyouare@gmail.com>
2022-09-24 17:22:53 +0100

    Added Online badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,15 @@
 import star from '../assets/star.png'
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.country.toLowerCase() === 'online') {
+    badgeText = "ONLINE"
+  }
   return (
       <div className="card">
-        {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+        {badgeText && <div className="card--badge">{badgeText}</div>}
         <img className="card--image" src={props.img} />
         <div className="card--info">
           <img className="card--star" src={star} alt="star" />
